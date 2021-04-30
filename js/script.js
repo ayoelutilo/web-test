ef0897e578e4e4ebdbdea6f4ac02b99fce191dc5
ENIGMA <40527644+ayoelutilo@users.noreply.github.com>
2021-04-30 14:31:46 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,5 +13,33 @@ fetch('https://loripsum.net/api')
         console.log('error')
     })
 
+$(document).ready(function() {
+
+    var $window = $(window);
+
+    function checkWindowWidth() {
+
+        var windowsize = $window.width();
+
+        if (windowsize > 1000) {
+
+            $('#navbar').css('display', 'block');
+
+        } else {
+
+            $('#navbar').css('display', 'none');
+
+        }
+    }
+
+    checkWindowWidth();
+
+    $(window).resize(checkWindowWidth);
+
+});
 
 
+$('#navbar-btn').click(function() {
+    
+    $('#navbar').toggle();
+});
